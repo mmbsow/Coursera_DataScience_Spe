0edb7513cc98b1ca60f73e42407fe73f3c1485b6
mmbsow <mmbsow@gmail.com>
2014-08-23 17:42:21 -0400

    remove phantom directory

--- ExploratoryAnalysis/Project/plot2png.R
+++
@@ -1,7 +0,0 @@
-# function printing a ggplot2 graph to a png file
-plot2png <- function(theplot, name, ...) {
-    name <- paste0(name, ".png")
-    png(filename=name)
-    print(theplot)
-    dev.off()
-} #plotting function
